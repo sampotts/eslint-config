e9c8ea6801e8368725c2b873117f1d00e0c604f0
Sam Potts <sam@potts.es>
2020-04-30 08:40:35 +1000

    Tweaks

--- react-ts.js
+++ react-ts.js
@@ -3,16 +3,9 @@
  */
 
 module.exports = {
-  extends: [
-    'eslint-config-airbnb-typescript',
-    '@sampotts/eslint-config/config/ts',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-    '@sampotts/eslint-config/rules/react',
-  ].map(require.resolve),
+  extends: ['eslint-config-airbnb-typescript', '@sampotts/eslint-config/config/ts', 'prettier/@typescript-eslint', '@sampotts/eslint-config/rules/react'].map(
+    require.resolve,
+  ),
   plugins: [],
   rules: {},
 };
